f540a5e04b93c184bb117baffa4657724fa86917
Anis <khalil-anis.zabat.1@ens.etsmtl.ca>
2022-12-06 10:19:10 -0500

    Added security for password

--- deleter.py
+++ deleter.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import getpass
 # Set up the web driver
 driver = webdriver.Chrome()
 
 # Go to the Google Chrome sign-in page
 driver.get('https://accounts.google.com/signin/chrome/sync')
 
+#Ask user for email and password
+email = input('Enter your email: ')
+    
+# hide the password as it is typed
+password = getpass.getpass('Enter your password: ')
+
 # Enter the email address and password
 email_field = driver.find_element(By.ID, 'identifierId')
-email_field.send_keys('your_email@example.com')
+email_field.send_keys(email)
 email_field.submit()
 password_field = driver.find_element(By.NAME, 'password')
-password_field.send_keys('your_password')
+password_field.send_keys(password)
 password_field.submit()
+
+# wait 15 seconds for the page to load
+driver.implicitly_wait(15)
 
 # Go to the passwords page
 driver.get('https://passwords.google.com')
