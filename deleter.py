ab8e731d75f3a8d35b655e70b10563ef65e7b3ef
Anis <Runebrun3@hotmail.com>
2022-12-06 09:24:06 -0500

    Finished first version

--- deleter.py
+++ deleter.py
@@ -1,21 +1,28 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-# Open the browser
-browser = webdriver.GoogleChrome()
+# Set up the web driver
+driver = webdriver.Chrome()
 
-# Open google account settings passwords
-browser.get('chrome://settings/passwords')
+# Go to the Google Chrome sign-in page
+driver.get('https://accounts.google.com/signin/chrome/sync')
 
-# Find the delete button for each saved password
-delete_buttons = browser.find_elements(By.XPATH, '//button[@aria-label="Delete"]')
+# Enter the email address and password
+email_field = driver.find_element(By.ID, 'identifierId')
+email_field.send_keys('your_email@example.com')
+email_field.submit()
+password_field = driver.find_element(By.NAME, 'password')
+password_field.send_keys('your_password')
+password_field.submit()
 
+# Go to the passwords page
+driver.get('https://passwords.google.com')
 
-# Click the delete button for each saved password
-for button in delete_buttons:
-    button.click()
+# Delete all saved passwords
+password_list = driver.find_elements(By.CLASS_NAME, 'password-entry')
+for password in password_list:
+    password.find_element(By.CLASS_NAME, 'password-entry-delete').click()
+    driver.find_element(By.XPATH, '//*[@id="password-delete-confirm"]/div/button[2]').click()
 
-
-
-
+# Close the web driver
+driver.close()
